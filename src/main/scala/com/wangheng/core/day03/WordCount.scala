72951f69a3429111ce45698b1852f3be760ceed7
wangheng <673602165@qq.com>
2019-04-04 09:48:12 +0800

    update

--- src/main/scala/com/wangheng/core/day03/WordCount.scala
+++ src/main/scala/com/wangheng/core/day03/WordCount.scala
@@ -4,9 +4,10 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 object WordCount {
   def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setAppName("wordcount")
+    val conf = new SparkConf().setAppName("wordcount").setMaster("local")
     val sc = new SparkContext(conf)
-    val lines = sc.textFile("hdfs://localhost:9000/spark_test_data.txt", 1)
+    sc.setLogLevel("WARN")
+    val lines = sc.textFile("hdfs://dell:9000/spark_test_data.txt", 1)
     val words = lines.flatMap(line=>line.split(" "))
     val pairs = words.map(word => (word, 1))
     val wordCounts = pairs.reduceByKey(_ + _)
